28b4e941c0d5a02456243659db49b33106078d0a
Radik Masagutov <radik121@mail.ru>
2022-08-17 10:04:53 +0300

    Omail processing optimization

--- Mail.py
+++ Mail.py
@@ -17,20 +17,23 @@ def mail_data():
     # date_message = [date.CreationTime.date() for date in messages][-2::]   # даты последних 2-х сообщений
     # if str(date_message[0]) == str(date_message[1]) == yesterday:
     messages.Sort("[ReceivedTime]", True)
+    yesterday_message = [i for i in messages if
+                         str(i.CreationTime.date()) == yesterday]  # берем только вчерашние сообщения
 
     # Сохраняем вложения последних 2-х сообщений
-    for message in messages:
+    for message in yesterday_message:
+        pattern = r'\d+/\d+/\d+.?,?\d+.?/?\d+\s?\w+'
+        # pattern = r'\d+/\d+/\d+.?,?\d+/\d+/\d.?,?\d+'
         for attachment in message.Attachments:
-            if str(message.ReceivedTime.date()) == yesterday:
-                # print(attachment.FileName)
-                attachment.SaveAsFile(os.path.join(path, str(attachment.FileName)))
-    print('Файлы загружены!')
-
-    # Данные из тела письма
-    pattern = r'\d+/\d+/\d+.?,?\d+.?/?\d+\s?\w+'
-    # pattern = r'\d+/\d+/\d+.?,?\d+/\d+/\d.?,?\d+'
-    novomol = [re.findall(pattern, i.body) for i in messages if 'Якшин' in i.To][0][0]
-    aviapark = [re.findall(pattern, i.body) for i in messages if 'Sergey Yakshin' in i.To][0][0]
+            # print(bool(attachment.FileName))
+            attachment.SaveAsFile(os.path.join(path, str(attachment.FileName)))
+            print(f'Файл {attachment.FileName} загружен!')
+        # Данные из тела письма
+        if 'новосибирск' in str(message.Sender) and len(re.findall(pattern, message.body)) != 0:
+            novomol = re.findall(pattern, message.body)[0]
+        if 'Пламида' in str(message.Sender) and len(re.findall(pattern, message.body)) != 0:
+            aviapark = re.findall(pattern, message.body)[0]
+    # print(novomol, aviapark)
 
     res = []
     for i in [re.split('[ /]', novomol), re.split('[ /]', aviapark)]:
@@ -41,8 +44,6 @@ def mail_data():
         res.append(i)
 
     return {'1323': res[0], '1324': res[1]}
-    # else:
-    #     return "Дата отчетов не совпадает!"
 
 
 if __name__ == '__main__':
@@ -50,5 +51,3 @@ if __name__ == '__main__':
 
     # Файлы загружены!
     # {'1323': ['947', '486', '663600', '2716'], '1324': ['978', '439', '403855', '2612']}
-
-
